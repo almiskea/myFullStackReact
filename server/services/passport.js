03020b5b5bca6472331b122f07cc113a838ea587
almiskea <ali.miskeen@gmail.com>
2017-12-17 18:55:58 -0800

    adding users

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,18 +1,19 @@
 const passport = require('passport');
-const key =  require('../config/keys');
+const mongoose = require('mongoose');
+const keys =  require('../config/keys');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID:'789083793219-5feqg69cpp54lej1pdv7pv3vqo3clb5u.apps.googleusercontent.com',
-      clientSecret:'MQYC18BjjJoWjduNk59lscXP',
+      clientID:keys.googleClientID,
+      clientSecret:keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken',accessToken);
-      console.log('refreshToken',refreshToken);
-      console.log('profile',profile);
+      new User({ googleId: profile.id }).save();
     }
   )
 );
